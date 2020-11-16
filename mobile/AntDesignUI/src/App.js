0674d0023d9e8c8c8d5db0db7a77a7a8c9c53375
thuantv11 <vinhthuan.tr@gmail.com>
2020-11-16 17:20:28 +0700

    # khoi tao app ==> thanh cong

---
+++ mobile/AntDesignUI/src/App.js
@@ -0,0 +1,48 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+    SafeAreaView,
+    StyleSheet,
+    ScrollView,
+    View,
+    Text,
+    StatusBar,
+} from 'react-native';
+
+import {
+    Header,
+    LearnMoreLinks,
+    Colors,
+    DebugInstructions,
+    ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+const App: () => React$Node = () => {
+    return (
+        <>
+            <StatusBar barStyle="dark-content" />
+            <SafeAreaView>
+                <ScrollView
+                    contentInsetAdjustmentBehavior="automatic"
+                    style={{flexGrow:1}}>
+                    <View style={styles.body}>
+                        <Text>{'HELLO'}</Text>
+                    </View>
+                </ScrollView>
+            </SafeAreaView>
+        </>
+    );
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default App;
